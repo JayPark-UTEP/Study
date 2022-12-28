7cf32afed5f6af046441c32a9ddfbb0a23255151
JayPark-UTEP <jpark8@miners.utep.edu>
2022-12-28 00:02:51 -0700

    protect port info

---
+++ React/boiler-plate/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mogngoURI: process.env.MONGO_URI
+}
--- React/boiler-plate/server/index.js
+++ React/boiler-plate/server/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({extended: ture}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://JayPark:Park9285@cluster0.auri531.mongodb.net/?retryWrites=true&w=majority' 
+mongoose.connect(config.mongoURI 
   // {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndMondify: false}
 ).then(() => console.log('MongoDB Connected...'))
  .catch(err => console.log(err))
@@ -13,13 +21,16 @@ mongoose.connect('mongodb+srv://JayPark:Park9285@cluster0.auri531.mongodb.net/?r
 app.get('/', (req,res) => res.send('Hello World! Welcome'))
 
 app.post('/register', (req, res) => {
-  //required infor for registeration from client
+  //required info for registeration from client
 
 
   const user = new User(req.body)
 
   user.save((err,userInfo) => {
-    
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
   })
 })
 
